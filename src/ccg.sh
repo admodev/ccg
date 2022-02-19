12bd261bf422a0c42e11538440115b511ae8045d
admodev <adolfomoyano@exegesisba.com>
2022-02-19 20:16:38 -0300

    add commit function

--- src/ccg.sh
+++ src/ccg.sh
@@ -28,6 +28,39 @@ error() { log "ERROR: $*" >&2; }
 fatal() { error "$*"; exit 1; }
 usage_fatal() { error "$*"; usage >&2; exit 1; }
 
+# Unique ID generator for commits:
+uuid()
+{
+    local N B T
+
+    for (( N=0; N < 16; ++N ))
+    do
+        B=$(( $RANDOM%255 ))
+
+        if (( N == 6 ))
+        then
+            printf '4%x' $(( B%15 ))
+        elif (( N == 8 ))
+        then
+            local C='89ab'
+            printf '%c%x' ${C:$(( $RANDOM%${#C} )):1} $(( B%15 ))
+        else
+            printf '%02x' $B
+        fi
+
+        for T in 3 5 7 9
+        do
+            if (( T == N ))
+            then
+                printf '-'
+                break
+            fi
+        done
+    done
+
+    echo
+}
+
 if [ $# -eq 0 ]
 then
     usage
@@ -49,5 +82,13 @@ else
     if [ $1 == "status" ]
     then
         ls
+    fi
+
+    if [ $1 == "commit" ]
+    then
+        commit_id="$(uuid)"
+        read -p "Enter commit message: " commit_message
+        echo "Created commit with id: $commit_id"
+        echo "Commit message: $commit_message"
     fi
 fi
